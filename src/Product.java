9a79764d6d3acacdfd21e4329d84cb20d54517cf
NihadHub <ouachournihad@gmail.com>
2025-11-07 17:16:52 +0100

    saving

---
+++ src/Product.java
@@ -0,0 +1,12 @@
+public class Product {
+        int stoke;
+        String productName;
+        Double price;
+        Product(int stoke, String productName,Double price){
+            this.stoke = stoke;
+            this.productName = productName;
+            this.price = price;
+        }
+    }
+
+
